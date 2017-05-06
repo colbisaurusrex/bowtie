700f0effd53b1d1808e009aae2ee970af3854241
colbisaurusrex <c.barthelmess1@gmail.com>
2017-05-06 12:23:19 -0700

    simulate network request to render progress component

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'material-ui/Tabs';
+import { Tabs, Tab, CircularProgress } from 'material-ui';
 import SideBar from './Sidebar.jsx';
 import Event from './Event.jsx';
 import sampleData from '../sample.js';
@@ -9,13 +9,17 @@ export default class Dashboard extends Component {
     super(props);
     this.state = {
       events: sampleData,
+      fetching: true,
     };
   }
   componentDidMount() {
+    // SIMULATION OF NETWORK REQUEST
+    setTimeout(() => {
+      this.setState({ fetching: false });
+    }, 2000);
     // TODO: I think this is crux of optimization. Should I request all events at once and hydrate the state
     // with the response? Or somehow pagiate and request them as the user interacts? How does this effect the attending
     // and hosting tabs?
-    console.log(this.props);
     console.log('send network request for all events ');
   }
 
@@ -23,21 +27,27 @@ export default class Dashboard extends Component {
   render() {
     return (
       <div>
-        <SideBar />
-        <Tabs>
-          <Tab label="All Events" />
-          <Tab label="Attending" />
-          <Tab label="Hosting" />
-        </Tabs>
-        {this.state.events.map(event => (
+        {this.state.fetching ?
+          <CircularProgress size={80} thickness={5} /> :
+          <div>
+            <SideBar />
+            <Tabs>
+              <Tab label="All Events" />
+              <Tab label="Attending" />
+              <Tab label="Hosting" />
+            </Tabs>
+            {this.state.events.map(event => (
           // TODO: Add keys to uniquely identify each event
-          <Event
-            title={event.title}
-            description={event.description}
-          />
+              <Event
+                title={event.title}
+                description={event.description}
+              />
          ))}
+          </div>
+        }
       </div>
     );
   }
+  // TODO: clear out all network requests, intervals, etc in a lifecycle method
 }
 // TODO: how do I type check for this.state.events
