82c4e1b9a863cd315b7ec7bd90d8565d7a9863b4
colbisaurusrex <c.barthelmess1@gmail.com>
2017-05-06 12:07:33 -0700

    add tab bar to filter events, remove lifecycle method

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'material-ui/Tabs';
 import SideBar from './Sidebar.jsx';
 import Event from './Event.jsx';
 import sampleData from '../sample.js';
@@ -10,11 +11,24 @@ export default class Dashboard extends Component {
       events: sampleData,
     };
   }
+  componentDidMount() {
+    // TODO: I think this is crux of optimization. Should I request all events at once and hydrate the state
+    // with the response? Or somehow pagiate and request them as the user interacts? How does this effect the attending
+    // and hosting tabs?
+    console.log(this.props);
+    console.log('send network request for all events ');
+  }
 
+  // TODO: nest button to open sidebar and tabs menu in a containing app bar
   render() {
     return (
       <div>
         <SideBar />
+        <Tabs>
+          <Tab label="All Events" />
+          <Tab label="Attending" />
+          <Tab label="Hosting" />
+        </Tabs>
         {this.state.events.map(event => (
           // TODO: Add keys to uniquely identify each event
           <Event
@@ -26,3 +40,4 @@ export default class Dashboard extends Component {
     );
   }
 }
+// TODO: how do I type check for this.state.events
